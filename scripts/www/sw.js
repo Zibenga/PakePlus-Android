226c9e338c965ffd02566b55f570d1f21ffc036a
Zibenga <139119475+Zibenga@users.noreply.github.com>
2026-05-25 23:11:46 +0800

    update src file from pakeplus

---
+++ scripts/www/sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'daily-v1';
+const FILES = ['/', '/index.html', '/css/style.css', '/js/main.js', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
